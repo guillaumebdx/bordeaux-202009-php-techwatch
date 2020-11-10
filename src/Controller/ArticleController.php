1a6ff89bc1d3d030111e8b4dec5bee1b72e69f53
Sandra Pla Libourel <sandraplalibourel@gmail.com>
2020-11-10 15:09:06 +0100

    Voir Plus section display article informations done

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -16,4 +16,34 @@ class ArticleController extends AbstractController
             header('Location: /');
         }
     }
+
+    public function articlesByDate()
+    {
+        $articleManagerByDate = new ArticleManager();
+
+        $articleViewByDate = $articleManagerByDate->getArticleOrderBy('created_at DESC');
+        return $this->twig->render('articles_by_date.html.twig', [
+            'article_date_view' => $articleViewByDate,
+        ]);
+    }
+
+    public function articlesByTrend()
+    {
+        $articleManagerTrend = new ArticleManager();
+
+        $articleViewByTrend = $articleManagerTrend->getArticleOrderBy('star DESC');
+        return $this->twig->render('articles_by_trend.html.twig', [
+            'article_trend_view' => $articleViewByTrend,
+        ]);
+    }
+
+    public function voirPlus($articleId)
+    {
+        $plusSection = new ArticleManager();
+        $plus = $plusSection->getArticleById($articleId);
+
+            return $this->twig->render('voir_plus.html.twig', [
+                'voir_plus' => $plus,
+            ]);
+    }
 }
--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -44,11 +44,30 @@ class ArticleManager extends AbstractManager
 
     public function getArticleOrderBy(string $what): array
     {
-        {
-            $query = "SELECT * FROM article 
-                        JOIN user ON user.id = article.user_id 
+            $query =   "SELECT a.id AS article_id, a.title, 
+                        a.image_url, a.article_url, a.description, 
+                        a.created_at, a.star
+                        FROM article a
+                        JOIN user ON user.id = a.user_id 
                         ORDER BY " . $what . " LIMIT " . self::CARD_NUMBER;
             return $this->pdo->query($query)->fetchAll();
-        }
+    }
+
+    public function getArticleById($articleId)
+    {
+        $query =   "SELECT a.id AS article_id, a.title, a.article_url, a.image_url, a.description, a.created_at, a.star, 
+                    u.username, 
+                    c.message, c.user_id
+                    FROM article a
+                    JOIN user u 
+                    ON u.id = a.user_id
+                    JOIN comment c
+                    ON a.id = c.article_id
+                    WHERE a.id =:id";
+
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue('id', $articleId, \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetch();
     }
 }
